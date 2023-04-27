1dbfdd0462818c014c5e6cc33a3db71e7c37c43f
Luisfe82 <luisfediana@gmail.com>
2023-04-26 22:42:49 -0600

    first commit and end commit

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+const { src, dest, watch, series, parallel } = require(`gulp`);
+ 
+//SCSS y SASS
+const sass = require(`gulp-sass`)(require(`sass`));
+const postcss = require(`gulp-postcss`);
+const autoprefixer = require(`autoprefixer`);
+ 
+//IMAGENES
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif')
+ 
+function css( done ) {
+  //compilar sass pasos
+  //1 identificar archivo, 2 compilarla, 3 guardar el css
+ 
+  src(`src/scss/app.scss`)
+    .pipe( sass() )
+    .pipe( postcss ([autoprefixer()]) )
+    .pipe( dest(`build/css`) )
+ 
+  done();
+}
+ 
+function imagenes () {
+  return src ('src/img/**/*') 
+    .pipe (imagemin ({optimizationLevel: 3}) )
+    .pipe ( dest ('build/img') );
+}
+ 
+function versionWebp () {
+  return src('src/img/**/*.{png,jpg}')
+    .pipe ( webp())
+    .pipe ( dest ('build/img'))
+}
+
+function versionAvif() {
+    const opciones = {
+        quality: 50
+    }
+    return src('src/img/**/*.{png,jpg}')
+        .pipe ( avif( opciones ))
+        .pipe ( dest ('build/img'))
+}
+ 
+function dev() {
+  watch ('src/scss/**/*.scss',css);
+  watch ('src/img/**/*',imagenes);
+}
+ 
+ 
+exports.css = css;
+exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.default = series (imagenes, versionWebp, versionAvif , css, dev);
+
+// series - Se inicia un tarea, y hasta que finaliza, inicica la siguinte
+// parallel - Todas inician al mismo tiempo
